2b23b802909d8cf501ca9176ea44ba1c74d92797
Ward Muylaert <ward.muylaert@gmail.com>
2014-10-18 21:03:35 +0200

    Find type of an expression

--- Main.hs
+++ Main.hs
@@ -1,6 +1,28 @@
 module Main where
 
-import Parser
+import qualified Parser as P
 
+data Type = Bool | Nat | BadlyTyped
+    deriving (Show, Eq)
 
-main = getContents >>= print . Parser.calc . Parser.lexer
+-- Parse
+--main = getContents >>= print . P.calc . P.lexer
+-- Parse and find type
+main = getContents >>= print . getType . P.calc . P.lexer
+
+getType :: P.Exp -> Type
+getType (P.Bbool P.Btrue) = Bool
+getType (P.Bbool P.Bfalse) = Bool
+getType (P.If c t f)
+    | getType c == Bool && getType t == getType f = getType t
+    | otherwise = BadlyTyped
+getType P.Zero = Nat
+getType (P.Succ e)
+    | getType e == Nat = Nat
+    | otherwise = BadlyTyped
+getType (P.Pred e)
+    | getType e == Nat = Nat
+    | otherwise = BadlyTyped
+getType (P.Iszero e)
+    | getType e == Nat = Bool
+    | otherwise = BadlyTyped
