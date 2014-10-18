6c55fa9f868a895529e240983b9bb4ffc3b5fb36
Ward Muylaert <ward.muylaert@gmail.com>
2014-10-18 22:06:17 +0200

    Evaluator function
    Probably needs to be rewritten, but decent proof of concept start.

--- Main.hs
+++ Main.hs
@@ -7,10 +7,13 @@ data Type = Bool | Nat | BadlyTyped
 
 main = do
     content <- getContents
+    let parsed = P.calc . P.lexer $ content
     putStr "Parsed: "
-    print . P.calc . P.lexer $ content
+    print parsed
     putStr "Type: "
-    print . getType . P.calc . P.lexer $ content
+    print . getType $ parsed
+    putStr "Evaluated: "
+    print . evaluate $ parsed
 
 getType :: P.Exp -> Type
 getType (P.Bbool P.Btrue) = Bool
@@ -28,3 +31,26 @@ getType (P.Pred e)
 getType (P.Iszero e)
     | getType e == Nat = Bool
     | otherwise = BadlyTyped
+
+-- TODO: Rewrite to more strictly follow the rules as described
+--       Idea: Rerun eval on it all, only executing a rule at the time
+--       If we're done, return what we have
+evaluate :: P.Exp -> P.Exp
+evaluate (P.Bbool P.Btrue) = P.Bbool P.Btrue
+evaluate (P.Bbool P.Bfalse) = P.Bbool P.Bfalse
+evaluate (P.Zero) = P.Zero
+evaluate (P.Iszero e) = case evaluate e of
+    P.Zero -> P.Bbool P.Btrue
+    P.Succ _ -> P.Bbool P.Bfalse
+    otherwise -> error "Evaluation failed"
+evaluate (P.Succ e) = case evaluate e of
+    P.Zero -> P.Succ P.Zero
+    P.Succ _ -> P.Succ $ evaluate e
+    otherwise -> error "Evaluation failed"
+evaluate (P.Pred e) = case evaluate e of
+    P.Zero -> P.Zero
+    P.Succ ev -> ev
+    otherwise -> error "Evaluation failed"
+evaluate (P.If c t f) = case evaluate c of
+    P.Bbool P.Btrue -> evaluate t
+    P.Bbool P.Bfalse -> evaluate f
